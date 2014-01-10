a67ff5f7d76e722c9023a68841b617c9b9515b06
laetitia massa <laetitiamassa@gmail.com>
2014-01-10 15:15:29 +0100

    added Amazon S3

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.0.0'
 
 gem 'devise', '~> 3.1.1'
 gem 'paperclip', '~> 3.0'
+gem 'aws-sdk', '~> 1.20.0'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,15 @@ Pinteresting::Application.configure do
   #required for Heroku
   #set to the actual domain name in production
   config.action_mailer.default_url_options = { :host => 'http://laet-pinteresting.herokuapp.com/' }
+
+  #set Paperclip to upload images to AWS S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
